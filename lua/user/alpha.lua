a8ca4064e9d0805bf60ca5c7e547ff05cdafd049
Warren Madden <svurg@yahoo.com>
2024-04-02 05:48:37 +0000

    Change "c" to respect the NVIM_APPNAME method of calling nvim
    The original version hardcoded editing ~/.config/nvim/init.lua
    
    This is changed to use stdpath('config') instead of ~/.config/nvim

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ function M.config()
     button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
     button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
     button("t", icons.ui.Text .. " Find text", ":Telescope live_grep <CR>"),
-    button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+    button("c", icons.ui.Gear .. " Config", ":exe 'edit '.stdpath('config').'/init.lua' <CR>"),
     button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
   }
   local function footer()
