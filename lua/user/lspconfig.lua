77b262f3f6e818a3a4eaf8d597bce2884d077a64
Warren Madden <svurg@yahoo.com>
2024-05-04 17:40:42 +0000

    updated inlay_hint calls for neovim function signature change

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -23,7 +23,7 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
 
   if client.supports_method "textDocument/inlayHint" then
-    vim.lsp.inlay_hint.enable(true, nil, bufnr)
+    vim.lsp.inlay_hint.enable(true, {bufnr})
   end
 
   if client.name == 'ruff_lsp' then
@@ -40,7 +40,7 @@ end
 
 M.toggle_inlay_hints = function()
   local bufnr = vim.api.nvim_get_current_buf()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(bufnr), nil, bufnr)
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({bufnr}), {bufnr})
 end
 
 function M.config()
