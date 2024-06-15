234d9670efbc4e1ca95db062552bdee1e1e50963
Warren Madden <svurg@yahoo.com>
2024-06-15 19:00:30 +0000

    fixed stripping CRs during paste from Windows clipboard

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -61,8 +61,8 @@ if vim.fn.has "wsl" then
       ["*"] = "clip.exe",
     },
     paste = {
-      ["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
-      ["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+      ["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('\r', ''))",
+      ["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('\r', ''))",
     },
     cache_enabled = 0,
   }
